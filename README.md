8893aa97c69bc0017540ec97e66d4936357f38af
Omer Benzer <omer.benzer@tum.de>
2023-12-14 16:47:53 +0100

    v1

---
+++ README.md
@@ -0,0 +1,10 @@
+# Debian12 Configure
+
+### Description
+My Debian Bookworm configuration script after install
+
+### Usage
+- some small changes are most likely necessary
+- run as root i.e. `sudo ./debian12-configure.sh`
+
+### [Copyright](LICENSE)
---
+++ debian12-configure.sh
@@ -0,0 +1,67 @@
+USERNAME="ocb"
+
+# Huawei Matebook D16 Microphone issue
+echo "pactl load-module module-detect" > /etc/profile.d/microphone-solution.sh
+
+# Update GRUB, uncomment if you have dual-boot
+sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
+sed -i 's/GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=true/' /etc/default/grub
+update-grub
+
+apt update
+apt install wget apt-transport-https ca-certificates curl gnupg
+
+pushd /home/$USERNAME/Downloads
+    # Docker GPG @ref https://docs.docker.com/engine/install/debian/
+    install -m 0755 -d /etc/apt/keyrings
+
+    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    chmod 644 /etc/apt/keyrings/docker.gpg
+
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian bookworm stable" \
+        > /etc/apt/sources.list.d/docker.list
+
+    # kubectl GPG @ref https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/#install-using-native-package-management
+    curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+    chmod 644 /etc/apt/keyrings/docker.gpg
+
+    echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' \
+        > /etc/apt/sources.list.d/kubernetes.list
+
+    # VSCode GPG
+    curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /etc/apt/keyrings/microsoft-archive-keyring.gpg
+    chmod 644 /etc/apt/keyrings/docker.gpg
+
+    echo "deb [arch=amd64,arm64,armhf signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main" \
+        > /etc/apt/sources.list.d/vscode.list
+popd
+
+apt update
+apt install -y \
+    gcc gdb cmake clangd \
+    maven default-jdk \
+    python3 python3-ipykernel \
+    kde-standard  openssh-server \
+    neofetch neovim nala \
+    docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
+    kubectl code \
+    flatpak plasma-discover-backend-flatpak
+
+# Post installation
+groupadd docker
+usermod -aG docker $USERNAME
+
+echo
+echo .. Done!
+echo you might want check if everything is ok:
+echo check "VSCode extensions"
+echo run "'sudo dpkg-reconfigure lightdm'" and select sddm for kde-plasma as your default desktop environment
+echo run "'docker run hello-world'" after reboot
+
+echo install your favorite flatpak apps as non-root:\n
+for pkg in Flameshot Okular VLC Krita OnlyOffice Drawio Zoom Discord "Telegram Desktop" Steam;
+do
+    echo $pkg
+done
+
+echo remove unnecessary packages if you feel fancy
